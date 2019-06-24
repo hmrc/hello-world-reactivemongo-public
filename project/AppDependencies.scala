d35b06c463bc72514dde12e52f40ef5d0417b147
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2019-06-24 13:58:15 +0100

    Switch from play-reactivemongo to simple-reactivemongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,17 +4,18 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "play-reactivemongo" % "6.4.0",
-    "uk.gov.hmrc" %% "bootstrap-play-25"  % "4.9.0"
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.12.0-play-25",
+    "uk.gov.hmrc" %% "bootstrap-play-25"    % "4.9.0"
   )
 
   val test = Seq(
-    "org.scalatest"          %% "scalatest"                % "3.0.5"   % "test, it",
-    "com.typesafe.play"      %% "play-test"                % current   % "test, it",
-    "org.pegdown"            % "pegdown"                   % "1.6.0"   % "test, it",
+    "org.scalatest"          %% "scalatest"                % "3.0.5"         % "test, it",
+    "com.typesafe.play"      %% "play-test"                % current         % "test, it",
+    "org.pegdown"            % "pegdown"                   % "1.6.0"         % "test, it",
     "uk.gov.hmrc"            %% "service-integration-test" % "0.5.0-play-25" % "test, it",
-    "org.scalatestplus.play" %% "scalatestplus-play"       % "2.0.1"   % "test, it",
-    "org.mockito"            % "mockito-all"               % "1.10.19" % "test, it"
+    "org.scalatestplus.play" %% "scalatestplus-play"       % "2.0.1"         % "test, it",
+    "org.mockito"            % "mockito-all"               % "1.10.19"       % "test, it",
+    "uk.gov.hmrc"            %% "reactivemongo-test"       % "4.8.0-play-25" % "test, it"
   )
 
 }
